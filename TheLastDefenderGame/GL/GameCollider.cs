103abd3123e2cc004dcd96ee3cb5ccd129856ccb
sma02 <sma02022@outlook.com>
2023-07-29 10:51:03 +0500

    Removed unused functions from GameColllider class

--- TheLastDefenderGame/GL/GameCollider.cs
+++ TheLastDefenderGame/GL/GameCollider.cs
@@ -13,23 +13,6 @@ namespace TheLastDefenderGame.GL
         {
             this.game = game;
         }
-        public bool CollisionWithPlayer()
-        {
-            Player player = game.Player;
-            GameCell nextCell;
-            GameObjectType type;
-            foreach (var value in Enum.GetValues(typeof(GameDirection)))
-            {
-                nextCell = player.CurrentCell.NextCell((GameDirection)value);
-                type = nextCell.CurrentGameObject.GameObjectType;
-                if (type == GameObjectType.ENEMY || type == GameObjectType.FIREABLE)
-                {
-                    return true;
-                }
-
-            }
-            return false;
-        }
         public List<Fireable> CollidingFirables()
         {
             List<Fireable> fireables = new List<Fireable>();
@@ -51,42 +34,6 @@ namespace TheLastDefenderGame.GL
                     fireables.Add(fireable);
                 }
             }
-        }
-        public int FireableCollisionsWithEnemy()
-        {
-            int score = 0;
-            for (int i = 0; i < game.Enemies.Count; i++)
-            {
-                Enemy enemy = game.Enemies[i];
-                GameCell nextCell;
-                GameObjectType type;
-                foreach (var value in Enum.GetValues(typeof(GameDirection)))
-                {
-                    nextCell = enemy.CurrentCell.NextCell((GameDirection)value);
-                    type = nextCell.CurrentGameObject.GameObjectType;
-                    if (type == GameObjectType.FIREABLE)
-                    {
-                        if (enemy.GetType() == typeof(Tank))
-                        {
-                            score += 3;
-                        }
-                        else if (enemy.GetType() == typeof(Cannon))
-                        {
-                            score += 1;
-                        }
-                        Fireable fireable = (Fireable)nextCell.CurrentGameObject;
-                        fireable.OwningCombatant.RemoveFireable(fireable);
-                        enemy.Health -= 30;
-                        if (enemy.Health <= 0)
-                        {
-                            game.RemoveEnemy(enemy);
-                        }
-                        i--;
-                    }
-
-                }
-            }
-            return score;
         }
     }
 }
